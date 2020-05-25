f1939c80a01003757ac1e21e7bfb5fc10334fb73
cooldhruv <41069975+cooldhruv@users.noreply.github.com>
2020-05-25 09:36:39 +0530

    Update creator.js

--- creator.js
+++ creator.js
@@ -1,25 +1,32 @@
-var a = document.getElementById('myF').addEventListener('click', addPersonal());
+var a = document.getElementById('myF').addEventListener('click', addPersonal);
 
-var b = document.getElementById('bt2').value = 1;
+//var b = document.getElementById('bt2').addEventListener('click',display);
 
-function addPersonal(){
+
+
+
+function addPersonal(e){
+	var ar = new Array();
 	var name = document.getElementById('firstName').value;
 	var contact = document.getElementById('c1').value;
 	var email = document.getElementById('email1').value;
 	var address = document.getElementById('address1').value;
 	var other  = document.getElementById('op1').value;
 
-	var obj = {
-		name: name,
-		contact: contact,
-		email: email,
-		address: address,
-		other: other
-	}
+	ar.push(name);
+	ar.push(contact);
+	ar.push(email);
+	ar.push(address);
+	ar.push(other);
+	
+	 localStorage.setItem('session',JSON.stringify(ar));
 
-	localStorage.setItem('obj',JSON.stringify(obj));
-
-
-}
-
+	
+	// document.getElementById('name2').innerHTML = name;
+	// document.getElementById('name2').innerHTML = contact;
+	// document.getElementById('contact2').innerHTML = email;
+	// document.getElementById('address2').innerHTML = address;
+	// document.getElementById('op2').innerHTML = other;
+	e.preventDefault();
+ }
 
